a47482c1856d7a356bad3a014e8fbb4c268a51c9
Areum Jo <areumjo1@gmail.com>
2019-08-08 16:54:38 -0400

    Added functional btns and displayed ball/strike

--- baseball-stadium-personnel/src/App.js
+++ baseball-stadium-personnel/src/App.js
@@ -1,15 +1,56 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [ball, setBall] = useState(0);
+  const [strike, setStrike] = useState(0);
+
+  const strikeBtn = () => {
+    if (strike === 2) {
+      setStrike(0)
+      setBall(0)
+    } else {
+      setStrike(strike + 1)
+    }
+  }
+  const ballBtn = () => {
+    if (ball === 3) {
+      setStrike(0)
+      setBall(0)
+    } else {
+      setBall(ball + 1)
+    }
+  }
+  const foulBtn = () => {
+    if (strike === 2) {
+      setStrike(2)
+    } else {
+      setStrike(strike + 1)
+    }
+  }
+  const hitBtn = () => {
+    setStrike(0);
+    setBall(0)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <p>
+        <h2>
           Baseball Stadium Pesonnel
-        </p>
+        </h2>
       </header>
-      <p>Hi</p>
+      <div className="display">
+        <h3>Display</h3>
+        <p>Ball: {ball}</p>
+        <p>Strike: {strike}</p>
+      </div>
+      <div className="dashboard">
+        <button onClick={strikeBtn}>Strike</button>
+        <button onClick={ballBtn}>Ball</button>
+        <button onClick={foulBtn}>Foul</button>
+        <button onClick={hitBtn}>Hit</button>
+      </div>
     </div>
   );
 }
