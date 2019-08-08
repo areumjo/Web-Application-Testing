1b4043c9e2676276a6223b0cd6e99f64577fd644
Areum Jo <areumjo1@gmail.com>
2019-08-08 15:55:12 -0400

    Initial commit: Created react app and added testing-library

---
+++ baseball-stadium-personnel/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Baseball Stadium Pesonnel
+        </p>
+      </header>
+      <p>Hi</p>
+    </div>
+  );
+}
+
+export default App;
